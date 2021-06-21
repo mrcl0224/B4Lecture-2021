ab48a0c40d4b494741899ca258a18a16b1ca53bf
mrcl0224 <mrclmacaroon.4c7@gmail.com>
2021-06-22 04:10:09 +0900

    Fix Style

--- ex_08_niwa/a_niwa/ex_08_niwa.py
+++ ex_08_niwa/a_niwa/ex_08_niwa.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import time
 
+
 def forward(outputs, PI, A, B):
 
     size, t = outputs.shape
@@ -16,11 +17,12 @@ def forward(outputs, PI, A, B):
 
         for j in range(1, t):
 
-            alpha = B[:, :, outputs[i, j]] * np.sum(A.T*alpha.T, axis = 1).T
+            alpha = B[:, :, outputs[i, j]] * np.sum(A.T * alpha.T, axis=1).T
 
         models[i] = np.argmax(np.sum(alpha, axis=1))
 
     return models
+
 
 def viterbi(outputs, PI, A, B):
 
@@ -33,26 +35,27 @@ def viterbi(outputs, PI, A, B):
 
         for j in range(1, t):
 
-            alpha = B[:, :, outputs[i, j]] * np.max(A.T*alpha.T, axis = 1).T
+            alpha = B[:, :, outputs[i, j]] * np.max(A.T * alpha.T, axis=1).T
 
         models[i] = np.argmax(np.max(alpha, axis=1))
 
     return models
+
 
 def main():
 
     args = sys.argv
 
     data = pickle.load(open(args[1], "rb"))
-    #print(data)
+    # print(data)
 
-    answer = np.array(data['answer_models'])
-    outputs = np.array(data['output'])
-    PI = np.array(data['models']['PI'])
-    A = np.array(data['models']['A'])
-    B = np.array(data['models']['B'])
+    answer = np.array(data["answer_models"])
+    outputs = np.array(data["output"])
+    PI = np.array(data["models"]["PI"])
+    A = np.array(data["models"]["A"])
+    B = np.array(data["models"]["B"])
 
-    #print("answer : \n", answer)
+    # print("answer : \n", answer)
 
     point1 = time.perf_counter()
 
@@ -68,13 +71,17 @@ def main():
     print("Forward Algorithm : {}".format(point2 - point1))
     print("Viterbi Algorithm : {}".format(point3 - point2))
 
-    #print(models_forward)
-    #print(np.count_nonzero(models_forward == answer)/models_forward.shape[0])
+    # print(models_forward)
+    # print(np.count_nonzero(models_forward == answer)/models_forward.shape[0])
 
-    acc_forward = np.count_nonzero(models_forward == answer)/models_forward.shape[0] * 100
+    acc_forward = (
+        np.count_nonzero(models_forward == answer) / models_forward.shape[0] * 100
+    )
     conf_mat_forward = np.zeros([A.shape[0], A.shape[0]])
 
-    acc_viterbi = np.count_nonzero(models_viterbi == answer)/models_viterbi.shape[0] * 100
+    acc_viterbi = (
+        np.count_nonzero(models_viterbi == answer) / models_viterbi.shape[0] * 100
+    )
     conf_mat_viterbi = np.zeros([A.shape[0], A.shape[0]])
 
     for i in range(answer.shape[0]):
@@ -97,6 +104,7 @@ def main():
     plt.ylabel("Actual Model")
     plt.xlabel("Predict Model")
     plt.show()
+
 
 if __name__ == "__main__":
 
